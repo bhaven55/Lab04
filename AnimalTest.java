c916d90c23ae55093613dea22a153102914ff7ab
Bhaven Patel <bhaven.s.patel-1@ou.edu>
2019-02-09 11:37:22 -0600

    All constructors work for the Animal Class

--- AnimalTest.java
+++ AnimalTest.java
@@ -10,6 +10,12 @@ public class AnimalTest
     /**
      * Test the empty Animal constructor and the getters
      */
+	
+	public final String COLOR = "Blue";
+	public final String NAME = "Tiger";
+	public final double HEIGHT = 7.0;
+	public final double WEIGHT = 225.6;
+	
     public void emptyConstructorTest() throws AssertException
     {
         // Use the default constructor
@@ -23,5 +29,17 @@ public class AnimalTest
     }
 
     // TODO: test full constructor, getters, and toString
+    public void testAnimalStringStringDoubleDouble() throws AssertException
+	{
+		// Uses the full constructor
+		Animal animal = new Animal(COLOR, NAME, WEIGHT, HEIGHT);
+		
+		// the name should be tiger, color should be blue, weight should be 225.6, height should be 7.0
+		Assert.assertEquals("Blue", animal.getColor());
+		Assert.assertEquals("Tiger", animal.getName());
+		Assert.assertEquals(225.6, animal.getWeight(), 0.01);
+		Assert.assertEquals(7.0, animal.getHeight(), 0.01);
+			
+	}
 }
 
