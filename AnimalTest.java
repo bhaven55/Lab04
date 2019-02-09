2cd1b1c32479a278e9a541060dab9589beb3e39b
Bhaven Patel <bhaven.s.patel-1@ou.edu>
2019-02-09 12:01:45 -0600

    The animal Class Works perfectly

--- AnimalTest.java
+++ AnimalTest.java
@@ -39,7 +39,65 @@ public class AnimalTest
 		Assert.assertEquals("Tiger", animal.getName());
 		Assert.assertEquals(225.6, animal.getWeight(), 0.01);
 		Assert.assertEquals(7.0, animal.getHeight(), 0.01);
-			
+	}
+    
+    public void testGetColor() throws AssertException
+	{
+		Animal animal = new Animal(COLOR, NAME, WEIGHT, HEIGHT);
+		String expected = "Blue";
+		String actual = animal.getColor();
+		
+		Assert.assertEquals(expected, actual);
+	}
+    
+    public void testGetName() throws AssertException 
+	{
+		Animal animal = new Animal(COLOR, NAME, WEIGHT, HEIGHT);
+		String expected = "Tiger";
+		String actual = animal.getName();
+		
+		Assert.assertEquals(expected, actual);
+	}
+    
+	public void testGetWeight() throws AssertException 
+	{
+		Animal animal = new Animal(COLOR, NAME, WEIGHT, HEIGHT);
+		double expected = 225.6;
+		double actual = animal.getWeight();
+		
+		Assert.assertEquals(expected, actual, 0.01);
+	}
+	
+	public void testGetHeight() throws AssertException 
+	{
+		Animal animal = new Animal(COLOR, NAME, WEIGHT, HEIGHT);
+		double expected = 7.0;
+		double actual = animal.getHeight();
+		
+		Assert.assertEquals(expected, actual, 0.01);
+	}
+	
+	public void testToString() throws AssertException 
+	{
+		Animal animal = new Animal(COLOR, NAME, WEIGHT, HEIGHT);
+		String expected = "Tiger, a Blue-colored animal. 225.6 pounds, 7.0 inches\n";
+		String actual = animal.toString();
+		
+		Assert.assertEquals(expected, actual);
+	}
+	
+	public void testEqualsObject() throws AssertException 
+	{
+		Animal animal = new Animal(COLOR, NAME, WEIGHT, HEIGHT);
+		Animal animal2 = new Animal("Red", "Monkey", 25.5, 7.0);
+		Animal animal3 = new Animal("Red", "Tiger", 225.6, 7.0);
+		Animal animal4 = new Animal("Blue", "Tiger", 225.6, 7.0);
+		
+		Assert.assertEquals(true, animal.equals(animal));
+		Assert.assertEquals(false, animal.equals(animal2));
+		Assert.assertEquals(false, animal.equals(animal3));
+		Assert.assertEquals(true, animal.equals(animal4));
+		
 	}
 }
 
