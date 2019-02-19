10055e9fd31490dfcc5bd7021d1da6ddfd4a984e
Bhaven Patel <bhaven.s.patel-1@ou.edu>
2019-02-19 17:46:49 -0600

    Files added

--- Todo.java
+++ Todo.java
@@ -1,18 +1,33 @@
 public class Todo {/*
-Lab Section_________: XX
-Last Modified_______: 20XX-XX-XX
-OU ID_______________: XXXXXXXXX
-OU EMAIL____________: XXX@ou.edu
+Lab Section_________: 12
+Last Modified_______: 2019-02-09
+OU ID_______________: 113483352
+OU EMAIL____________: bhaven.s.patel-1@ou.edu
 
 Tasks:
-1. XXX
-2. XXX
+1. Test constructors for the animals class
+2. Test methods for the animals class
+3. Test constructors for the Zoo class
+4. Test methods for the animals class
+5. Test constructors for the Equipment class
+6. Test methods for the Equipment class
+7. finish the implementation of the equals()
 
 Estimated Time (minutes):
-1. XXX
-2. XXx
+1. 5
+2. 25
+3. 5
+4. 25
+5. 5
+6. 25
+7. 30
 
 Actual Time (minutes):
-1. XXX
-2. XXX
+1. 7
+2. 10
+3. 25
+4. 120
+5. 5
+6. 30
+7. 5
 */}
